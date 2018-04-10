14397d9c128bf9199b9fc9eb0cd9508b66c74b50
zuozhiw <zuozhiw@gmail.com>
2018-04-10 14:20:59 -0700

    change default password to be consistent

--- src/StarServlet.java
+++ src/StarServlet.java
@@ -25,7 +25,7 @@ public class StarServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // change this to your own mysql username and password
 		String loginUser = "mytestuser";
-        String loginPasswd = "test";
+        String loginPasswd = "mypassword";
         String loginUrl = "jdbc:mysql://localhost:3306/moviedb";
 		
         // set response mime type
